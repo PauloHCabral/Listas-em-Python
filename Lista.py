f21bce397d981b300f6886d2f10c7ebee7377e01
PauloHCabral <61445141+PauloHCabral@users.noreply.github.com>
2024-10-31 23:19:31 -0300

    Create Lista.py

---
+++ Lista.py
@@ -0,0 +1,45 @@
+biblia = [
+    "Genises",
+    "Exodo",
+    "Levítico",
+    "Números",
+    "Deuteronômio",
+    "Josue",
+    "Juízes",
+    "Rute",
+    "1 Samuel",
+    "2 Samuel",
+    "1 Reis",
+    "2 Reis",
+    "1 Cronicas",
+    "2 Cronicas",
+    "Esdras",
+    "Neemias",
+    "Ester",
+    "Jó",
+    "Salmos",
+    "Eclesiastes",
+    "Cântico dos Cânticos",
+    "Isaias",
+    "Jeremias",
+    "Lamentações",
+    "Ezequial",
+    "Daniel",
+    "Oseias",
+    "Joel",
+    "Amós",
+    "Obadias",
+    "Jonas"
+    "Miqueias",
+    "Naum",
+    "Habacuque"
+    "Sofonias",
+    "Ageu",
+    "Zacarias",
+    "Malaquias"
+    
+]
+
+print(type(biblia))
+
+primeiro_elemento = print(biblia[0])
